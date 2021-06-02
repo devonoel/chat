5a1a9b6d42c6a6324de8d2516ae2e096b06b262a
Devon Noel de Tilly <devon.noeldetilly@gmail.com>
2021-06-02 14:03:12 -0400

    Add a couple seed channels

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Channel.destroy_all
+Channel.create!(name: 'general')
+Channel.create!(name: 'random')
