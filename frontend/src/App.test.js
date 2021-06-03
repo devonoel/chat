0e408ac2792adba77a4cd247ec06fdd25edcb1f9
Devon Noel de Tilly <devon.noeldetilly@gmail.com>
2021-06-03 13:10:53 -0400

    Add a test for fetching channels from API

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -1,8 +1,9 @@
 import { render, screen } from '@testing-library/react';
+import { act } from "react-dom/test-utils";
 import App from './App';
 
 test('renders learn react link', () => {
-  render(<App />);
+  act(() => { render(<App />) });
   const headerElement = screen.getByText(/chat app/i);
   expect(headerElement).toBeInTheDocument();
 });
--- frontend/src/components/ChannelList/ChannelList.test.js
+++ frontend/src/components/ChannelList/ChannelList.test.js
@@ -1,8 +1,21 @@
 import { render, screen } from '@testing-library/react';
+import { act } from "react-dom/test-utils";
 import ChannelList from './ChannelList';
 
 test('renders learn react link', () => {
-  render(<ChannelList />);
+  act(() => { render(<ChannelList />) });
   const headingElement = screen.getByText(/channels/i);
   expect(headingElement).toBeInTheDocument();
+});
+
+test('fetches channel list', async () => {
+  const fakeChannels = [{ id: 1, name: 'general' }];
+  jest.spyOn(global, "fetch").mockImplementation(() =>
+    Promise.resolve({
+      json: () => Promise.resolve(fakeChannels)
+    })
+  );
+
+  await act(async () => { render(<ChannelList />) });
+  expect(screen.getByText(/general/i));
 });
