36700f3f0d8fda7e9ea6d26c6b14c545d9b19297
Devon Noel de Tilly <devon.noeldetilly@gmail.com>
2021-06-03 14:52:15 -0400

    Start building input form for messages on Channel component

--- frontend/src/components/Channel/Channel.js
+++ frontend/src/components/Channel/Channel.js
@@ -1,22 +1,46 @@
-import "./Channel.css";
+import {useState, useEffect} from "react";
 import {Link} from "react-router-dom";
+import "./Channel.css";
 
 function Channel(props) {
   let channelName;
 
-  if (props.location && props.location.channelProps.name) {
+  if (props.location && props.location.channelProps) {
     channelName = props.location.channelProps.name;
   } else {
     channelName = props.name;
   }
 
+  const [message, setMessage] = useState("");
+
+  // const [channels, setChannels] = useState([]);
+  //
+  // useEffect(() => { fetchChannels() }, []);
+
   return (
     <section className="Channel">
-      <h3>#{channelName}</h3>
+      <div className="Channel-heading">
+        <h3>#{channelName}</h3>
+        <Link to="/">Go Back</Link>
+      </div>
 
-      <Link to="/">Back</Link>
+      <div className="Channel-history"></div>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="message">Message:</label>
+        <input name="message" id="message" type="text" value={message} onChange={handleChange} />
+        <input type="submit" value="Send" />
+      </form>
     </section>
   )
+
+  function handleChange(e) {
+    setMessage(e.target.value);
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    console.log(message);
+  }
 }
 
 export default Channel;
--- frontend/src/components/Channel/Channel.test.js
+++ frontend/src/components/Channel/Channel.test.js
@@ -1,4 +1,5 @@
-import { render, screen } from '@testing-library/react';
+import React from "react";
+import { render, screen, fireEvent } from '@testing-library/react';
 import { act } from "react-dom/test-utils";
 import { BrowserRouter as Router } from "react-router-dom";
 import Channel from './Channel';
@@ -7,4 +8,19 @@ test('renders channel name', () => {
   act(() => { render(<Router><Channel name="example"/></Router>) });
   const headingElement = screen.getByText(/example/i);
   expect(headingElement).toBeInTheDocument();
+});
+
+test('updates message state when typing in text input', () => {
+  const setState = jest.fn();
+  const useStateSpy = jest.spyOn(React, 'useState');
+  useStateSpy.mockImplementation((init) => [init, setState]);
+
+  act(() => { render(<Router><Channel name="example"/></Router>) });
+  const textInput = screen.getByLabelText(/message/i);
+  expect(textInput.value).toBe("");
+
+  act(() => {
+    fireEvent.change(textInput, { target: { value: "foo" } });
+  });
+  expect(setState).toHaveBeenCalledWith("foo");
 });
